d9d91937e41e84cd23b8a5e506ee49934880c1fe
rakshith_s <rakshithsantosh@gmail.com>
2023-11-12 11:05:51 +0530

    event emitter

--- server.js
+++ server.js
@@ -7,8 +7,26 @@
 const {add,subtract,multiply,divide} = require('./math')//using destructuring instead of using math object
 /*const os = require('os');
 
-console.log(os.version())*/
+console.log(os.version())
 console.log(add(2,1))
 console.log(subtract(2,1))
 console.log(multiply(2,1))
-console.log(divide(2,1))
+console.log(divide(2,1))*/
+
+const logEvents = require('./logEvents')
+
+const EventEmitter = require('events')
+
+class MyEmitter extends EventEmitter {};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the lof event 
+myEmitter.on('log',(msg)=>{
+    logEvents(msg)
+})
+
+setTimeout(()=>{
+    myEmitter.emit('log','log event emitted')
+},2000)
