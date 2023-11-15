8a26fa267f36e9e90dc2a1052b858d5f64e7db9d
rakshith_s <rakshithsantosh@gmail.com>
2023-11-15 06:24:23 +0530

    middleware

--- express/server.js
+++ express/server.js
@@ -3,6 +3,16 @@ const app = express()
 const path = require('path')
 const PORT =process.env.PORT || 3500
 
+//built in middle ware to handle urlencoded data in other words , form data
+app.use(express.urlencoded({extended:false}))
+
+//built in middleware for json
+app.use(express.json())
+
+//to serve static files
+app.use(express.static(path.join(__dirname,'/public')))
+//console.log(__dirname)
+
 app.get('^/$|index(.html)?',(req,res)=>{
     //res.sendFile('./index.html',{root:__dirname})
     res.sendFile(path.join(__dirname,'index.html'))
@@ -31,3 +41,6 @@ app.get('/*',(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`server is started on port ${PORT}`)
 })
+
+//anything between the request and response is middle ware eg : route handlers
+// built in , custom and 3rd part middle ware
